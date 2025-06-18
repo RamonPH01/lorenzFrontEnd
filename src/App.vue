e5b0fdbf904a7a8e51ab7d7e3ca985f982a780ea
beck-johannes <j.beck@kuhn-elektro.de>
2025-06-18 15:28:26 +0200

    added font awesome icon

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-    <div class="font-mono">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" />
+
+  <div class="font-mono">
         <!-- Hier wird deine Home-Seite oder andere Seiten angezeigt -->
         <router-view />
     </div>
