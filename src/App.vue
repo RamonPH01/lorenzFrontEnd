ed266de5a042081212940ab937815a6dd67a0f7a
beck-johannes <j.beck@kuhn-elektro.de>
2025-06-20 16:18:56 +0200

    header and footer outsourced

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" />
-
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"/>
   <div class="font-mono">
-        <!-- Hier wird deine Home-Seite oder andere Seiten angezeigt -->
-        <router-view />
-    </div>
+    <Header/>
+    <router-view/>
+    <Footer/>
+  </div>
 </template>
 
-<script setup>
-// kein Script nötig, wenn keine Logik hier gebraucht wird
+
+<script setup lang="ts">
+import Header from "/src/views/header.vue"
+import Footer from "/src/views/footer.vue"
 </script>
---
+++ src/views/footer.vue
@@ -0,0 +1,5 @@
+<template>
+  <footer class="text-center bg-[#eacdb6] text-[#372c31] text-sm py-4">
+    © {{ new Date().getFullYear() }} Event App · Alle Rechte vorbehalten
+  </footer>
+</template>
