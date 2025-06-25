49fb910bcaeb7c830e2d0d89f30300fd807b219c
beck-johannes <j.beck@kuhn-elektro.de>
2025-06-25 11:59:39 +0200

    kleine aufräumarbeiten

--- src/views/host-login.vue
+++ src/views/host-login.vue
@@ -24,25 +24,26 @@ function handleLogin() {
         <div
             class="bg-[#d9b49c] shadow-md rounded-2xl border border-[#deb699] p-8 w-full max-w-sm"
         >
-            <h1 class="text-3xl font-bold text-[#4a2c2a] mb-6 text-center">
+            <h1 class="text-3xl font-bold text-[#4a2c2a] mb-3 text-center">
                 Host Login
             </h1>
-            <form @submit.prevent="handleLogin" class="flex flex-col gap-4">
+          <hr/>
+            <form @submit.prevent="handleLogin" class="flex flex-col gap-4 mt-3">
                 <input
                     v-model="username"
                     type="text"
-                    placeholder="Username"
-                    class="rounded-xl border border-[#deb699] p-3 focus:outline-none focus:ring-2 focus:ring-[#eacdb6]"
+                    placeholder="Benutzername"
+                    class="rounded-xl border border-[#eacdb6] p-3 focus:outline-none focus:ring-2 focus:ring-[#eacdb6]"
                 />
                 <input
                     v-model="password"
                     type="password"
-                    placeholder="Password"
-                    class="rounded-xl border border-[#deb699] p-3 focus:outline-none focus:ring-2 focus:ring-[#eacdb6]"
+                    placeholder="Passwort"
+                    class="rounded-xl border border-[#eacdb6] p-3 focus:outline-none focus:ring-2 focus:ring-[#eacdb6]"
                 />
                 <button
                     type="submit"
-                    class="bg-[#eacdb6] text-[#4a2c2a] font-semibold rounded-xl py-2 hover:bg-[#deb699] transition"
+                    class="bg-[#eacdb6] text-[#4a2c2a] font-semibold rounded-xl py-2 hover:bg-[#deb699] transition cursor-pointer"
                 >
                     Login
                 </button>
