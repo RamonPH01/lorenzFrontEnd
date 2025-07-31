e50bd4552644f88acb05f529e83bc6797f741fd0
beck-johannes <beckjohannes@icloud.com>
2025-07-31 21:10:47 +0200

    sichere env implementiert... ups

--- src/main.js
+++ src/main.js
@@ -10,13 +10,13 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyB5qwg1USEcA5G5-1g7BjGAuQCU2HIED34",
-    authDomain: "veranstalterlogin.firebaseapp.com",
-    projectId: "veranstalterlogin",
-    storageBucket: "veranstalterlogin.firebasestorage.app",
-    messagingSenderId: "930448388874",
-    appId: "1:930448388874:web:0bec7b7801e6221042c0f3",
-    measurementId: "G-PY1KGCQ0VC"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
